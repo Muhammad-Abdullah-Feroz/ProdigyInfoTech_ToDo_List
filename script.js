9611d11a4a802e9cb05ef2e5b0ce20d1ba068d3f
Muhammad Abdullah <abdullahferoz313@gmail.com>
2024-06-20 22:55:52 +0500

    Removed Test_Code_Lines and Extra_Comments from the code! Hopefully all of them

--- script.js
+++ script.js
@@ -5,35 +5,41 @@ const addBtn = document.querySelector(".front-top").querySelector("button")
 const taskSection = document.querySelector(".task-section")
 let tasks = []
 
-function fillTaskSection(){
+function fillTaskSection() {
     taskSection.innerHTML = "";
     for (const task of tasks) {
-        taskSection.innerHTML+=task;
+        taskSection.innerHTML += task;
     }
 }
 
-function addDeleteListener(){
-    Array.from(document.getElementsByClassName("del")).forEach((e,i)=>{
-        e.addEventListener("click",()=>{
-            console.log(e,i);
-            let index = i-1
-            tasks.splice(index,1)
+function addDeleteListener() {
+    Array.from(document.getElementsByClassName("del")).forEach((e, i) => {
+        e.addEventListener("click", () => {
+            console.log(e, i);
+            let index = i - 1
+            tasks.splice(index, 1)
             fillTaskSection();
             addDeleteListener()
         })
     })
 
-    Array.from(document.getElementsByClassName("status-btn")).forEach((e,i)=>{
-        e.addEventListener("click",()=>{
+    Array.from(document.getElementsByClassName("status-btn")).forEach((e, i) => {
+        e.addEventListener("click", () => {
             e.innerHTML = "Completed"
-            e.className+=" completed"
+            e.className += " completed"
+            setTimeout(() => {
+                let index = i - 1
+                tasks.splice(index, 1)
+                fillTaskSection();
+                addDeleteListener()
+            }, 2000);
         })
     })
 }
-addBtn.addEventListener("click",(e)=>{
-    if(input.value==""){
+addBtn.addEventListener("click", (e) => {
+    if (input.value == "") {
         console.warn(`No input yet`);
-    }else{
+    } else {
         console.log(input.value);
         let task = input.value
         let newTask = `
